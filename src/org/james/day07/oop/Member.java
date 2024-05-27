4b6ba3e2f3866be23c9bc6f93adaffe62f833bb8
user1 <user1@DESKTOP-FCD7JPV>
2024-05-27 17:50:18 +0900

    [ADD] 24/05/27 자바 7일차 클래스와 객체
    1. 클래스의 개념과 설계 실습
    2. 객체와 메모리 구조
     - 참조변수는 stack영역에, 객체는 heap영역에 동적할당
    3. 접근지정자(제한자)
     - public
     - private
     - default
     - protected
    4. static에 대한 이해
     - static은 공유된다. 공기와 같이, 같은 클래스로 만든 객체에 대해서..

---
+++ src/org/james/day07/oop/Member.java
@@ -0,0 +1,27 @@
+package org.james.day07.oop;
+
+import java.util.Scanner;
+
+public class Member {
+	
+	// 멤버변수(필드)
+	public String name;
+	char gender;
+	public int age;
+	String education;
+	long salary;
+	public String job;
+	String property;	// 집 1채, 자동차 2대 / 500000000
+	String phone;				// 01012345678
+	String address;
+	boolean divorceYN;	// Yes or No
+	String children;			// 1남 1여, 아들1, 딸1
+	
+	public Member() {} // 생성자
+	
+	// 멤버메소드(메소드)
+//	public void registerMember() {}
+	public void doHeart() {}
+	public void sendMessage() {}
+	public void doPromise() {}
+}
---
+++ src/org/james/day07/oop/exercise/Rectangle.java
@@ -0,0 +1,12 @@
+package org.james.day07.oop.exercise;
+
+public class Rectangle {
+	// 너비와 높이를 입력받아
+	// 사각형의 넓이를 출력하는 클래스를 만드시오.
+	public int rectangleWidth;
+	public int rectangleHeight;
+	
+	public int getArea() {
+		return rectangleWidth*rectangleHeight;
+	}
+}
