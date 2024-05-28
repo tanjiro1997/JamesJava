00a489c621e3d1878154b60fbfb433652260c1fb
user1 <user1@DESKTOP-FCD7JPV>
2024-05-28 17:35:36 +0900

    [ADD] 24/05/28 자바 8일차 객체지향 프로그래밍
    1. 절차지향 프로그래밍(ScoreProgram)
    2. 구조적 프로그래밍(ScoreProgram2)
    3. 객체지향 프로그래밍(ScoreProgram3)

---
+++ src/org/james/day08/oop/score/ScoreProgram2.java
@@ -0,0 +1,67 @@
+package org.james.day08.oop.score;
+
+import java.util.Scanner;
+
+public class ScoreProgram2 {
+	static int kor;
+	static int eng;
+	static int math;	// ScoreProgram2 클래스의 멤버필드
+	public static void main(String[] args) {
+		finish :
+		while(true) {
+			int choice = printMenu();
+			switch(choice) {
+			case 1 : 
+				inputScore();
+				break;
+			case 2 : 
+				printScore();
+				break;
+			case 3 : 
+				displayMsg("프로그램을 종료합니다.");
+//				System.out.println("프로그램을 종료합니다.");
+				break finish;
+			default : 
+				displayMsg("1 ~ 3 사이의 수를 입력해주세요.");
+//				System.out.println("1 ~ 3 사이의 수를 입력해주세요.");
+			}
+		}
+	}
+	private static void displayMsg(String message) {
+		System.out.println(message);
+		
+	}
+	// ScoreProgram2 클래스의 멤버메소드
+		private static void printScore() {
+			System.out.println("====== 성적 출력 ======");
+			System.out.println("국어 : " + kor);
+			System.out.println("영어 : " + eng);
+			System.out.println("수학 : " + math);
+			System.out.println("총합 : " + (kor+eng+math));
+			System.out.println("평균 : " + (kor+eng+math)/3.0);
+	}
+		
+		public static int printMenu() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("====== 메인 메뉴 ======");
+			System.out.println("1. 성적입력");
+			System.out.println("2. 성적출력");
+			System.out.println("3. 종료");
+			System.out.print("선택 >> ");
+			int choice = sc.nextInt();
+			return choice;
+		}
+		
+		public static void inputScore() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("====== 성적 입력 ======");
+			System.out.print("국어 : ");
+			kor = sc.nextInt();
+			System.out.print("영어 : ");
+			eng = sc.nextInt();
+			System.out.print("수학 : ");
+			math = sc.nextInt();
+		}
+	}
+
+
