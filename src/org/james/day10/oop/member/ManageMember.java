0ffad4ac894047604572fc0ef00d4ed3c8268aeb
user1 <user1@DESKTOP-79SC9CL>
2024-06-11 16:38:28 +0900

    [ADD] 24/05/30 자바 10일차 회원관리 객체지향
    1. 회원관리 프로그램 연습

---
+++ src/org/james/day10/oop/member/ManageMember.java
@@ -0,0 +1,35 @@
+package org.james.day10.oop.member;
+
+public class ManageMember {
+//	private Member member;
+	private Member [] members;
+	private int index;
+	
+	public ManageMember() {
+		members = new Member[5];
+		index = 0;
+	}
+	
+	public void registerMember(Member member) {
+//		this.member = member;
+		if(index < members.length) {
+			members[index] = member;
+			index++;
+		}
+	}
+
+	public Member [] allMember() {
+//		return member;
+		return members;
+	}
+
+	public Member searchOneById(String memberId) {
+		for(Member member : members) {
+			if(member != null && memberId.equals(member.getMemberId())) {
+				return member;
+			}
+		}
+		return null;
+	}
+
+}
