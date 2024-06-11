31db1e054345539865d7f8cd8a8b0adc376fb38f
user1 <user1@DESKTOP-79SC9CL>
2024-06-11 16:35:26 +0900

    [ADD] 24/05/29 자바 9일차 객체지향원칙 2, 상속
    1. 상속의 개념 및 목적 이해
    2. 상속을 사용하는 방법
     - extends 키워드 사용
    3. 접근지정자 protected에 대한 이해 및 실습

---
+++ src/org/james/day09/oop/inheritance/Exam_Inheritance.java
@@ -0,0 +1,16 @@
+package org.james.day09.oop.inheritance;
+
+import org.james.day09.oop.inheritance.point.ColorPoint;
+
+public class Exam_Inheritance {
+
+	public static void main(String[] args) {
+		ColorPoint cp = new ColorPoint(10, 23, "green");
+		cp.showColorPoint();
+//		ColorPoint cp = new ColorPoint();
+//		cp.setX(5);
+//		cp.setY(7);
+//		cp.showPoint();
+	}
+
+}
